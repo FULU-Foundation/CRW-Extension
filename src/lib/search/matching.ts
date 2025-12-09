cd8aadc602dc1a7d22788746ab3ef4268b29ee54
Pavel Lishnev <pavel.lishnev@ostealth.com>
2025-12-09 18:19:01 +0000

    Simply url searcch logic && Update watch command

--- src/lib/search/matching.ts
+++ src/lib/search/matching.ts
@@ -4,6 +4,7 @@ import {
   extractUrlTokens,
   extractFuseQuery,
   normalizeDataset,
+  getDomainRoot,
 } from "./matchingHelper";
 import Fuse, { FuseResult } from "fuse.js";
 import { FuseResultV2, UrlToken } from "./types";
@@ -64,44 +65,59 @@ const fuseOptions = {
 };
 
 export function matchByUrl(entiries: CargoEntry[], url: string): CargoEntry[] {
-  const urlTokens = extractUrlTokens(url);
-  const dataset = normalizeDataset(entiries);
+  const urlTokens: UrlToken[] = extractUrlTokens(url);
+  const dataset: CargoEntry[] = normalizeDataset(entiries);
+  const results: CargoEntry[] = [];
 
+  for (const item of dataset) {
+    urlTokens
+      .filter(token => token.source == "domain")
+      .map(token => {
+        const website = item?.['Website'] ?? ''
+        const domain = getDomainRoot(website)
+
+        if (domain === token.value) {
+          results.push(item);
+        }        
+      })
+  }
+
+  return results.slice(0, 3);
+
+  // Fuse.js
   // const fuse = new Fuse(dataset, fuseOptions);
   // const fusyQuery = extractFuseQuery(urlTokens);
   // console.log(`${Constants.LOG_PREFIX} Fuse.js queries: `, fusyQuery)
 
   // const combined = fuse.search(fusyQuery);
   // console.log(`${Constants.LOG_PREFIX} Fuse.js results: `, combined)
-
   // return combined.slice(0, 3).map((value) => value.item);
 
-  const scored = [];
+  // Fuse.js && Deterministic
+  // const scored = [];
+  // for (const item of dataset) {
+  //   let score = 0;
 
-  for (const item of dataset) {
-    let score = 0;
+  //   for (const token of urlTokens) {
+  //     if (!token) continue;
 
-    for (const token of urlTokens) {
-      if (!token) continue;
+  //     const fuse = new Fuse([item], fuseOptions);
 
-      const fuse = new Fuse([item], fuseOptions);
+  //     // Exact phrase match → high score
+  //     if (item.__searchable.includes(token.value)) {
+  //       score += token.weight;
+  //     }
 
-      // Exact phrase match → high score
-      if (item.__searchable.includes(token.value)) {
-        score += token.weight;
-      }
+  //     if (fuse.search(token.value).length > 0) {
+  //       score += token.weight;
+  //     }
+  //   }
 
-      if (fuse.search(token.value).length > 0) {
-        score += token.weight;
-      }
-    }
-
-    if (score > 0) {
-      scored.push({ item, score });
-    }
-  }
-  scored.sort((a, b) => b.score - a.score);
-  console.log(`${Constants.LOG_PREFIX} matchDeterministic results: `, scored);
-
-  return scored.slice(0, 3).map((value) => value.item);
+  //   if (score > 0) {
+  //     scored.push({ item, score });
+  //   }
+  // }
+  // scored.sort((a, b) => b.score - a.score);
+  // console.log(`${Constants.LOG_PREFIX} matchDeterministic results: `, scored);
+  // return scored.slice(0, 3).map((value) => value.item);
 }
